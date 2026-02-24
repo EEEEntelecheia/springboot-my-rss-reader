128529f9cd47b0ea11efc6b66143d8ae06c8192a
EEEEntelecheia <1583697577@qq.com>
2026-02-24 17:33:10 +0800

    Create a guide class

---
+++ src/main/java/com/eeeentelecheia/RssApplication.java
@@ -0,0 +1,14 @@
+package com.eeeentelecheia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 引导类。Springboot项目的入口
+ */
+@SpringBootApplication
+public class RssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RssApplication.class, args);
+    }
+}
