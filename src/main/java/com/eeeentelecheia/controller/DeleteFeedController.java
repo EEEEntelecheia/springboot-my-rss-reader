b7cd0d00eb29865ace0c4d60d1fc5329d5205df2
EEEEntelecheia <1583697577@qq.com>
2026-02-24 19:31:02 +0800

    create controller

---
+++ src/main/java/com/eeeentelecheia/controller/DeleteFeedController.java
@@ -0,0 +1,10 @@
+package com.eeeentelecheia.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeleteFeedController {
+    @RequestMapping()
+    public void deleteFeed() {}
+}
